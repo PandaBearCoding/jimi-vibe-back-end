6aca3effa20735571a36606ca90cde6b2da2b4cb
pandabearcoding <madelinerose16@outlook.com>
2021-01-14 01:02:49 -0500

    commit 6, week 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
+    # post '/login', to: 'auth#create'
+    get '/login', to: 'users#login'
+    get '/signup', to: 'users#create'
+    get "/users/:id/matches", to: "users#matches"
+    get "/users/:id/matches/:id", to: "matches#show"
+    post "/users/:id/matches", to: "matches#create"
+    delete "/users/:id/matches/:id", to: "matches#destroy"
+    delete "/logout", to: "users#logout"
+  end 
+
+  resources :products
+  end
 end
+
+
