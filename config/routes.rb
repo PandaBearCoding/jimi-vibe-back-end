dc5c88400c8770ca2cbc8a0653a1d3639da1d82b
pandabearcoding <madelinerose16@outlook.com>
2021-01-20 21:49:14 -0500

    week 2, commit 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,24 @@
 Rails.application.routes.draw do
 
-  resources :users
-    # post '/login', to: 'auth#create'
-    get '/login', to: 'users#login'
-    get '/signup', to: 'users#create'
-    delete "/logout", to: "users#logout"
-
-    get "/users/:id/favorites", to: "users#favorites"
-    get "/users/:id/favorites/:id", to: "favorites#show"
-    post "/users/:id/favorites", to: "favorites#create"
-    delete "/users/:id/favorites/:id", to: "favorites#destroy"
-
-    get "/users/:id/carts", to: "users#carts"
-    get "/users/:id/carts/:id", to: "carts#show"
-    post "/users/:id/carts", to: "carts#create"
-    delete "/users/:id/carts/:id", to: "carts#destroy"
-  end 
-
   resources :products
-  end
+  resources :users
+  #   # post '/login', to: 'auth#create'
+  #   get '/login', to: 'users#login'
+  #   get '/signup', to: 'users#create'
+  #   delete "/logout", to: "users#logout"
+
+  #   get "/users/:id/favorites", to: "users#favorites"
+  #   get "/users/:id/favorites/:id", to: "favorites#show"
+  #   post "/users/:id/favorites", to: "favorites#create"
+  #   delete "/users/:id/favorites/:id", to: "favorites#destroy"
+
+  #   get "/users/:id/carts", to: "users#carts"
+  #   get "/users/:id/carts/:id", to: "carts#show"
+  #   post "/users/:id/carts", to: "carts#create"
+  #   delete "/users/:id/carts/:id", to: "carts#destroy"
+  # end
 
 end
+
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+
+user1 = User.create(username: "mrs161", password: "abc123", name: "Madeline Rose Stalter", profile_picture: "https://cdn.mainichi.jp/vol1/2020/08/31/20200831p2g00m0fe122000p/8.jpg?1", email: "madelinerose16@outlook.com", phone_number: "315-663-7462", address: "87 Clay Street, Apt 10, Brooklyn, NY 11222", cc_number: "1111", cc_date: "12/24", cc_cvv: "111")
